6526b2130609c51b8115d03c71977c286a3e66ee
Nils <its.just.nils@gmail.com>
2017-02-02 09:05:24 +0100

    pipebuilder WIP

---
+++ src/World/PipeBuilder.js
@@ -0,0 +1,24 @@
+function PipeBuilder()
+{
+
+}
+
+PipeBuilder.prototype.start = function()
+{
+
+};
+
+PipeBuilder.prototype.stop = function()
+{
+
+};
+
+PipeBuilder.prototype._update = function()
+{
+
+};
+
+PipeBuilder.prototype._build = function()
+{
+
+};
